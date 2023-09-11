ccb0f89f624b361794070365a1fe12f129842fcc
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-09-11 21:28:09 +0300

    lesson1, learned how "get" work

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,65 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  let a = 3;
-  if (a > 4) {
-    res.send("OK");
-  } else {
-    res.send("Hello World!");
-  }
-});
 
-app.get("/samurais", (req, res) => {
-  res.send("Hello samsdsdei!");
-});
-app.post("/samurais", (req, res) => {
-  res.send("We hav created samurais!");
-});
+const products = [{id: 1, title: 'tomatto'}, {id: 1, title: 'orange'}]
+const adress = [{ id: 1, value: 'Dimitrova70'}, {id: 2, value: 'Polosy20'}]
+
+app.get('/products', (req, res)=>{
+  res.send(products)
+})
+app.get('/products/:id', (req, res)=>{
+  req.params.id
+  let product = products.find(s => s.id === +req.params.id)
+  if(product){
+    res.send(product);
+  } else{
+    res.send(404);
+  }
+
+  
+})
+app.get('/adress/:id', (req, res)=>{
+  req.params.id
+  let street = adress.find(s => s.id === +req.params.id)
+  if(street){
+    res.send(street)
+  } else{
+    res.send(404)
+  }
+  
+})
+// app.get('/adress/leninsky', (req, res)=>{
+//   let street = adress.find(s => s.value === 'Polosy 20')
+//   res.send(street)
+// })
+
+
+// app.get("/courses", (req, res) => {
+
+//     res.json([
+//       {id: 1, title: 'front'},
+//       {id: 2, title: 'Back'},
+//       {id: 3, title: 'devOps'},
+//       {id: 3, title: 'qa'},
+//     ]);
+// });
+// app.get("/courses/:id", (req, res) => {
+
+//   res.json([
+//     {id: 1, title: 'front'},
+//     {id: 2, title: 'Back'},
+//     {id: 3, title: 'devOps'},
+//     {id: 3, title: 'qa'},
+//   ].find(c => c.id === +req.params.id));
+// });
+
+// app.get("/samurais", (req, res) => {
+//   res.send("Hello samsdsdei!");
+// });
+// app.post("/samurais", (req, res) => {
+//   res.send("We hav created samurais!!!!@!@!");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
